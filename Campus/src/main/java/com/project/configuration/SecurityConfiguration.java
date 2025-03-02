928edbaf0c1b295f93881bb4fad37343d2c02e04
jeet-sde <sde.jeetjadav@gmail.com>
2025-03-02 23:29:44 +0530

    commit intial

--- Campus/src/main/java/com/project/configuration/SecurityConfiguration.java
+++ Campus/src/main/java/com/project/configuration/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -84,4 +85,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		InMemoryTokenRepositoryImpl memory = new InMemoryTokenRepositoryImpl();
 		return memory;
 	}
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
 }
